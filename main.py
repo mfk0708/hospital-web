4fe2f325d48736614954857e76cbecd92382cb40
mfk0708 <mfahadkhan8010@gmail.com>
2025-07-13 19:26:09 +0530

    added cross origin

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from fastapi import FastAPI
-from routers import dashboard,checkup,pathology,intake,prescription,count,patient
+from routers import dashboard, checkup, pathology, intake, prescription, count, patient
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 import os
-
+from fastapi.responses import Response
 
 load_dotenv()
 app = FastAPI()
 
-
 API_KEY = os.getenv("API_KEY")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://localhost:5174"],
+    allow_origins=["https://healthdash-app.netlify.app"],  # include protocol
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(dashboard.router)
 app.include_router(checkup.router)
@@ -27,5 +26,11 @@ app.include_router(pathology.router)
 app.include_router(intake.router)
 app.include_router(patient.router)
 
+@app.get("/")
+def read_root():
+    return {"message": "Hospital API is running"}
 
+@app.get("/favicon.ico")
+def favicon():
+    return Response(status_code=204)  # no content
 
